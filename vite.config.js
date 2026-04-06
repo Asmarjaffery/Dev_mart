1a8dde264845f838bdb61eb02878bbe89e190b1e
Iraj jaffery <asmarjaffery@gmail.com>
2026-04-06 07:46:22 +0500

    fix: trigger redeploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         target: 'https://api.groq.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/groq/, '')
+        
       }
     }
   }
